cebdc99ff1fee84096a386fc174bea4361b339a7
sakroitel <sakroitel100@gmail.com>
2024-04-16 14:14:13 +0300

    Title and model fix

--- tasks/models.py
+++ tasks/models.py
@@ -13,7 +13,7 @@ class Task(models.Model):
 
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='tasks')
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     due_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
